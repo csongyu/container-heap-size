55c4d5397a5e2c6a2cd236c2d47edd3487c264ef
csongyu <chensongyu1024@gmail.com>
2022-06-06 00:45:28 +0800

    use soft reference

--- src/main/java/xyz/csongyu/containerheapsize/AllocateHeapMemory.java
+++ src/main/java/xyz/csongyu/containerheapsize/AllocateHeapMemory.java
@@ -2,29 +2,30 @@ package xyz.csongyu.containerheapsize;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
+import java.lang.ref.SoftReference;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
-public class AllocateHeapMemory implements CommandLineRunner {
-    private static final List<byte[]> TOTAL = new ArrayList<>();
+public class AllocateHeapMemory {
+    private static final List<SoftReference<byte[]>> TOTAL = new ArrayList<>();
 
     @Value("${app.allocate-heap-size}")
     private int allocateHeapSize;
 
-    @Override
-    public void run(final String... args) {
+    @PostConstruct
+    public void allocate() {
         final int mb = 1024 * 1024;
-        final byte[] bytes = new byte[mb];
-        IntStream.range(0, this.allocateHeapSize).forEach(i -> TOTAL.add(bytes));
+        IntStream.range(0, this.allocateHeapSize).forEach(i -> TOTAL.add(new SoftReference<>(new byte[mb])));
 
         final MemoryMXBean memoryBean = ManagementFactory.getMemoryMXBean();
         final long used = memoryBean.getHeapMemoryUsage().getUsed() / mb;
